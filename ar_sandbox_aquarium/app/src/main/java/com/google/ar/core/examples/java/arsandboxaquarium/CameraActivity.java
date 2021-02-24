659af466647e280fe01675cec30895a384a58306
Jayleen Li <jayleenli17@gmail.com>
2021-02-24 02:02:41 -0800

    Graphic design is my passion with slightly misaligned buttons no worries, camera works but it saves bitmaps..

--- ar_sandbox_aquarium/app/src/main/java/com/google/ar/core/examples/java/arsandboxaquarium/CameraActivity.java
+++ ar_sandbox_aquarium/app/src/main/java/com/google/ar/core/examples/java/arsandboxaquarium/CameraActivity.java
@@ -50,7 +50,7 @@ public class CameraActivity extends Activity {
    */
   private static final int UI_ANIMATION_DELAY = 300;
   private final Handler mHideHandler = new Handler();
-  //private View mContentView;
+  private View mContentView;
   private final Runnable mHidePart2Runnable = new Runnable() {
     @SuppressLint("InlinedApi")
     @Override
@@ -60,15 +60,15 @@ public class CameraActivity extends Activity {
       // Note that some of these constants are new as of API 16 (Jelly Bean)
       // and API 19 (KitKat). It is safe to use them, as they are inlined
       // at compile-time and do nothing on earlier devices.
-//      mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-//              | View.SYSTEM_UI_FLAG_FULLSCREEN
-//              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+      mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
   };
-  //private View mControlsView;
+  private View mControlsView;
   private final Runnable mShowPart2Runnable = new Runnable() {
     @Override
     public void run() {
@@ -77,7 +77,7 @@ public class CameraActivity extends Activity {
 //      if (actionBar != null) {
 //        actionBar.show();
 //      }
-      //mControlsView.setVisibility(View.VISIBLE);
+      mControlsView.setVisibility(View.VISIBLE);
     }
   };
   private boolean mVisible;
@@ -113,13 +113,13 @@ public class CameraActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-
-    mVisible = true;
+    mVisible = false;
+    mContentView= getWindow().getDecorView();
 
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_camera);
-    this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+    this.imageView = (ImageView)this.findViewById(R.id.objectImageView);
     ImageButton photoButton = (ImageButton) this.findViewById(R.id.take_photo);
     photoButton.setOnClickListener(new View.OnClickListener()
     {
@@ -180,23 +180,11 @@ public class CameraActivity extends Activity {
 
   private void hide() {
     // Hide UI first
-    //mControlsView.setVisibility(View.GONE);
-    //mVisible = false;
+    mVisible = false;
 
     // Schedule a runnable to remove the status and navigation bar after a delay
     mHideHandler.removeCallbacks(mShowPart2Runnable);
     mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-  }
-
-  private void show() {
-    // Show the system bar
-//    mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-//    mVisible = true;
-
-    // Schedule a runnable to display UI elements after a delay
-    mHideHandler.removeCallbacks(mHidePart2Runnable);
-    mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
   }
 
   /**
